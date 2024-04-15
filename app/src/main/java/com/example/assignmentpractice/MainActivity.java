a353b4b8ced99c17cc76dd58b693974aca07aae0
2586491 <2586491@wits.students.ac.za>
2024-04-15 20:48:40 +0200

    A simple app to display inputs

--- app/src/main/java/com/example/assignmentpractice/MainActivity.java
+++ app/src/main/java/com/example/assignmentpractice/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.assignmentpractice;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import androidx.activity.EdgeToEdge;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn2;
+    Button removeBtn;
+    Button addBtn;
+    TextView displayText;
+    EditText contactNo;
+    EditText Email;
+    EditText someText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        removeBtn = findViewById(R.id.Remove);
+        addBtn = findViewById(R.id.Add);
+        displayText = findViewById(R.id.Display);
+        contactNo = findViewById(R.id.Contact);
+        Email = findViewById(R.id.Email);
+        someText = findViewById(R.id.Something);
     }
 
+    public void doDisplay(View v) {
+        String contact = contactNo.getText().toString();
+        String email = Email.getText().toString();
+        String some = someText.getText().toString();
+
+        String textAll = contact + "\n" + email + "\n" + some;
+
+        displayText.setText(textAll);
+
+        contactNo.setText("");
+        Email.setText("");
+        someText.setText("");
+    }
 }
